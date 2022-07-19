6f7dce1c42bc7d9649b311ebf1f3d7f09fb5dd0b
KamilWardyla <conax13@gmail.com>
2022-07-19 18:32:51 +0200

    Kostka

--- Deff-Kostka do Gry.py
+++ Deff-Kostka do Gry.py
@@ -1,75 +1,69 @@
 import random
 
+OPERATION = 0
+DICE_THROW = 1
+DICE_RANGE = 0
+Z = 0
 
-def variables_dice():
-    code = input("Podaj ciąg znaków opisujący rzut, format(xDy+z): ")
-    global index_d
-    index_d = 0
-    global index_operation
-    index_operation = 0
+
+class DiceRangeError(Exception):
+    pass
+
+
+"""
+x - liczba rzutów kostką
+y - rodzaj kostki
+z - opcjonalny modifikator
+"""
+
+
+def check_dice_equation():
+    code = input("Podaj ciąg znaków opisujący rzut, format(xDy+Z): ")
+    global OPERATION, DICE_THROW, DICE_RANGE, Z
+    index_of_operation = 0
     operation_list = ["+", "-"]
-    global dice_type
     dice_type = [3, 4, 6, 8, 10, 12, 20, 100]
-    global operation
-    operation = 0
-    global dice_throw
-    dice_throw = 1
-    global dice_range
-    dice_range = 0
-    global z
-    z = 0
-    i = 0
+    Z = 0
     if code.index("D") == 0:
-        index_d = 0
+        index_of_d = 0
     else:
-        index_d = code.index("D")
+        index_of_d = code.index("D")
+
     for op in code:
         if op in operation_list:
-            operation = op
-            index_operation = code.index(operation)
-            z = code[index_operation + 1:]
-        else:
-            pass
-    if index_d == 0:
-        pass
+            OPERATION = op
+            index_of_operation = code.index(OPERATION)
+            Z = code[index_of_operation + 1:]
+    if index_of_d != 0:
+        DICE_THROW = code[0:index_of_d]
+    if OPERATION != 0:
+        DICE_RANGE = code[index_of_d + 1:index_of_operation]
     else:
-        dice_throw = code[0:index_d]
-    if operation != 0:
-        dice_range = code[index_d + 1:index_operation]
-    else:
-        dice_range = code[index_d + 1:]
-    dice_throw = int(dice_throw)
-    dice_range = int(dice_range)
-    z = int(z)
-    if dice_range not in dice_type:
-        raise Exception("There is not such cube")
+        DICE_RANGE = code[index_of_d + 1:]
+    DICE_THROW = int(DICE_THROW)
+    DICE_RANGE = int(DICE_RANGE)
+    Z = int(Z)
+    if DICE_RANGE not in dice_type:
+        raise DiceRangeError("There is not such cube")
 
 
 def dice_game():
-    try:
-        variables_dice()
-        global result
-        result = 0
-        if operation == 0:
-            for i in range(0, dice_throw):
-                result += random.randint(1, dice_range)
-                print(f"Result to: {result}")
-                i += 1
-            return result
-        elif operation == "+":
-            for i in range(0, dice_throw):
-                result += random.randint(1, dice_range)
-                print(f"Result to: {result}")
-                i += 1
-            return result + z
-        elif operation == "-":
-            for i in range(0, dice_throw):
-                result += random.randint(1, dice_range)
-                print(f"Result to: {result}")
-                i += 1
-            return result - z
-    except:
-        return "Podaj poprawny kod"
+    global OPERATION
+    throw_types = {"0": throw_dice, "+": lambda: throw_dice() + Z, "-": lambda: throw_dice() - Z}
+    while True:
+        try:
+            check_dice_equation()
+            return throw_types[str(OPERATION)]()
+        except DiceRangeError:
+            print("Podaj poprawny kod")
+
+
+def throw_dice():
+    result = 0
+    for i in range(0, DICE_THROW):
+        result += random.randint(1, DICE_RANGE)
+        print(f"Result to: {result}")
+    return result
 
 
 print(dice_game())
---
+++ Kostka.py
@@ -0,0 +1,1 @@
+def check_dice_equation():
